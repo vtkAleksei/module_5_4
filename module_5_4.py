7e1a293a8d6386cf5534b5a7df709aae3044855a
vtkAleksei <79051905+vtkAleksei@users.noreply.github.com>
2024-11-18 00:51:29 +0400

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,29 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+print(House.houses_history)
+del h3
+print(House.houses_history)
+
+
